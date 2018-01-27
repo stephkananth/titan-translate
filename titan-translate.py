a5956e75f0b56b1a6d9f631213744f8beca0dc99
Steph <sananth@andrew.cmu.edu>
2018-01-27 15:59:02 -0500

    spanish to english

--- titan-translate.py
+++ titan-translate.py
@@ -1,7 +1,14 @@
 from subprocess import Popen, PIPE
+from googletrans import Translator
 
 pipe = Popen('tesseract image.png stdout', shell=True, stdout=PIPE).stdout
 output = pipe.read().decode("utf-8").strip()
 
 print()
 print(output)
+
+translator = Translator()
+print(translator.translate(output))
+print(type(translator.translate(output)))
+
+
